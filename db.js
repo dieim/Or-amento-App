75a1c2e90e05c9c2b4da70fdfbdac82ff1cd0f95
dieim <57272737+dieim@users.noreply.github.com>
2026-01-08 14:05:54 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,33 +1,61 @@
 let db;
 
-const request = indexedDB.open("orcamentoDB", 1);
+const request = indexedDB.open("orcamentoDB", 2);
 
-request.onupgradeneeded = function(event) {
+request.onupgradeneeded = function (event) {
     db = event.target.result;
+
+    // Store de despesas
     if (!db.objectStoreNames.contains("despesas")) {
-        db.createObjectStore("despesas", { keyPath: "id", autoIncrement: true });
+        const store = db.createObjectStore("despesas", { keyPath: "id", autoIncrement: true });
+        store.createIndex("categoria", "categoria", { unique: false });
+        store.createIndex("data", "data", { unique: false });
+    } else {
+        const store = event.target.transaction.objectStore("despesas");
+
+        if (!store.indexNames.contains("categoria")) {
+            store.createIndex("categoria", "categoria", { unique: false });
+        }
+        if (!store.indexNames.contains("data")) {
+            store.createIndex("data", "data", { unique: false });
+        }
+    }
+
+    // Store de categorias personalizadas
+    if (!db.objectStoreNames.contains("categorias")) {
+        db.createObjectStore("categorias", { keyPath: "id", autoIncrement: true });
     }
 };
 
-request.onsuccess = function(event) {
+request.onsuccess = function (event) {
     db = event.target.result;
-    if (typeof carregarDespesas === "function") {
-        carregarDespesas();
-    }
+    if (typeof carregarDespesas === "function") carregarDespesas();
+    if (typeof carregarCategorias === "function") carregarCategorias();
 };
 
-request.onerror = function() {
-    console.log("Erro ao abrir IndexedDB");
-};
-
-function salvarDespesa(desc, valor) {
+function salvarDespesa(desc, valor, categoria, data) {
     const tx = db.transaction("despesas", "readwrite");
-    tx.objectStore("despesas").add({ descricao: desc, valor: parseFloat(valor) });
+    tx.objectStore("despesas").add({
+        descricao: desc,
+        valor: parseFloat(valor),
+        categoria,
+        data
+    });
 }
 
 function listarDespesas(callback) {
     const tx = db.transaction("despesas", "readonly");
-    const store = tx.objectStore("despesas");
-    const req = store.getAll();
+    const req = tx.objectStore("despesas").getAll();
+    req.onsuccess = () => callback(req.result || []);
+}
+
+function adicionarCategoria(nome) {
+    const tx = db.transaction("categorias", "readwrite");
+    tx.objectStore("categorias").add({ nome });
+}
+
+function listarCategorias(callback) {
+    const tx = db.transaction("categorias", "readonly");
+    const req = tx.objectStore("categorias").getAll();
     req.onsuccess = () => callback(req.result || []);
 }
